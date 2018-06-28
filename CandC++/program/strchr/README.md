6021d54beb752d73cc571bd966ff2231793639e0
YankunLi <lioveni99@gmail.com>
2018-06-28 11:25:24 +0800

    add strchr function

---
+++ CandC++/program/strchr/README.md
@@ -0,0 +1,9 @@
+#
+The function of strchr()
+***
+**Header File**: string.h
+**Prototype**: `extern char *strchr(const char *s, char c)`
+**Parameter**:
+    *s: string pointer
+    *c: char to needed found
+
---
+++ CandC++/program/strchr/strchr.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    const static char *str_ptr = "where is the char";
+    const static char ch = 'r';
+    const char * chr_ptr = NULL;
+
+    chr_ptr = strchr(str_ptr, ch);
+    if (chr_ptr) {
+        printf("string is : %s\n", str_ptr);
+        printf("The index of char %c is %d \n", ch, (chr_ptr - str_ptr + 1));
+    } else {
+        fprintf(stderr, "don't found char %c in %s\n", ch, str_ptr);
+    }
+
+    return 0;
+}
